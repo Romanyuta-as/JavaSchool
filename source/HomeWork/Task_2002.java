32d3721e0955332a59dd51edeb933efb29cab148
Romanyuta-as <Syber18@mail.ru>
2016-07-17 14:40:33 +0400

    ее

--- source/HomeWork/Task_2002.java
+++ source/HomeWork/Task_2002.java
@@ -5,7 +5,7 @@ package HomeWork;
  */
 public class Task_2002 {
     public static void main(String[] args){
-        int[] n = {1,5,12,45,15};
+        int[] n = {1, 5,12,45,15};
         int sum=0;
         for(int i=0; i<5; i++)
             sum = sum+n[i];
